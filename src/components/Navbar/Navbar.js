ebc4c4ca7f221f9659f2052f40f90ed245c96b84
Neha <nehapansare007@gmail.com>
2025-03-04 14:10:38 +0530

    Navabr responsiveness added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import './Navbar.css';
@@ -6,6 +6,7 @@ import './Navbar.css';
 function Navbar() {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const userStatus = localStorage.getItem('isLoggedIn') === 'true';
@@ -19,27 +20,38 @@ function Navbar() {
     navigate('/login'); // Redirect to Login
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar">
       {/* Brand Name */}
       <h1>
-        <span className='brand-name1'>COLO</span>
-        <span className='brand-name2'>SHOP</span>
+        <span className="brand-name1">COLO</span>
+        <span className="brand-name2">SHOP</span>
       </h1>
 
+      {/* Hamburger Menu Icon (visible on small screens) */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
       {/* Navigation Links */}
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/designer">Designer</Link>
-        <Link to="/contact">Contact Us</Link>
+      <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={() => setIsMenuOpen(false)}>Home</Link>
+        <Link to="/shop" onClick={() => setIsMenuOpen(false)}>Shop</Link>
+        <Link to="/blog" onClick={() => setIsMenuOpen(false)}>Blog</Link>
+        <Link to="/designer" onClick={() => setIsMenuOpen(false)}>Designer</Link>
+        <Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact Us</Link>
       </div>
 
       {/* Login/Logout Box */}
-      <div className='login-box'>
+      <div className="login-box">
         {isLoggedIn ? (
-          <p onClick={handleLogout} >Logout</p>
+          <p onClick={handleLogout}>Logout</p>
         ) : (
           <Link to="/login">Login</Link>
         )}
